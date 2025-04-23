02352f79139433465132b3ab1f7e08ba23747618
MigueLA207 <mariasmarin981@gmail.com>
2025-04-22 21:52:31 -0500

    Add files via upload

--- validacion_de_productos.py
+++ validacion_de_productos.py
@@ -2,85 +2,90 @@ print("╔═══════════════════════�
 print("║                    VALIDACIÓN DE PRODUCTOS                  ║") 
 print("╚═════════════════════════════════════════════════════════════╝\n")
 
-porcentaje_descuento=0
-#Verificamos el nombre del producto
+
+productos = []
+
+
 while True:
-    try:
-        nombre_producto = str(input("Ingresa el nombre del producto: "))
 
-        if nombre_producto.isalpha():
-            break
-        else: 
-            print("Ingrese un nombre valido")            
-           
-    except ValueError:
-        print("ingresa un dato valido por favor")
+    porcentaje_descuento=0
+    #Verificamos el nombre del producto
+    while True:
+        try:
+            nombre_producto = str(input("Ingresa el nombre del producto: "))
 
-
-#Verificamos el precio_unitario
-while True:
-    try:
-        precio_unitario = float(input("Ingresa el precio del producto: "))
-
-        if precio_unitario >=0:
-            break
-        else: 
-            print("Ingrese un valor positivo")            
-           
-    except ValueError:
-        print("ingresa un dato valido por favor")
+            if nombre_producto.isalpha():
+                break
+            else: 
+                print("Ingrese un nombre valido")            
+            
+        except ValueError:
+            print("ingresa un dato valido por favor")
 
 
-#Verificamos el precio_unitario
-while True:
-    try:
-        cantidad_productos = int(input("Ingresa la cantidad de productos: "))
-        if cantidad_productos >= 0:
-            break
-        else: 
-            print("Ingrese un valor positivo")            
-           
-    except ValueError:
-        print("ingresa un dato valido por favor")
-        
+    #Verificamos el precio_unitario
+    while True:
+        try:
+            precio_unitario = float(input("Ingresa el precio del producto: "))
 
+            if precio_unitario > 0:
+                break
+            else: 
+                print("Ingrese un valor positivo")            
+            
+        except ValueError:
+            print("ingresa un dato valido por favor")
+
+
+    #Verificamos el precio_unitario
+    while True:
+        try:
+            cantidad_productos = int(input("Ingresa la cantidad de productos: "))
+            if cantidad_productos >= 0:
+                break
+            else: 
+                print("Ingrese un valor positivo")            
+            
+        except ValueError:
+            print("ingresa un dato valido por favor")
+            
+
+
+    #Verificamos que el porcentaje sea valido
+    while True:
+        try:
+            porcentaje_descuento = int(input("Ingresa el porcentaje de descuento a aplicar(0% - 100%): "))
+            if porcentaje_descuento >=1 and porcentaje_descuento <=100 or porcentaje_descuento == 0:
+                break 
+            else: 
+                print("Ingrese un valor en el rango de 0 - 100")            
+            
+        except ValueError:
+            print("ingresa un dato valido por favor")  
+    
+    #Lo agrega como tupla
+    productos.append((nombre_producto,precio_unitario,cantidad_productos))
+
+
+    seguir = int(input("\nDigite 1 si quiere agregar mas nombres, digite 2 si quiere salir del programa:  "))
+    if seguir == 2:
+        break
 
 #Verificamos que el porcentaje sea valido
-while True:
-    try:
-        porcentaje_descuento = int(input("Ingresa el porcentaje de descuento a aplicar(0% - 100%): "))
-        if porcentaje_descuento >=1 and porcentaje_descuento <=100 or porcentaje_descuento == 0:
-            break 
-        else: 
-            print("Ingrese un valor en el rango de 0 - 100")            
-        
-    except ValueError:
-        print("ingresa un dato valido por favor")  
- 
+    while True:
+        try:
+            porcentaje_descuento = int(input("Ingresa el porcentaje de descuento a aplicar(0% - 100%): "))
+            if porcentaje_descuento >=1 and porcentaje_descuento <=100 or porcentaje_descuento == 0:
+                break 
+            else: 
+                print("Ingrese un valor en el rango de 0 - 100")            
+            
+        except ValueError:
+            print("ingresa un dato valido por favor")
+
+total = 0
+for valor in productos:
+    print(valor[0],valor[1],valor[2])
 
 
-
-#Calculamos el total
-
-print("\n╔═════════════════════════════════════════════════════════════╗") 
-print("║                          FACTURA                            ║") 
-print("╚═════════════════════════════════════════════════════════════╝\n")
-
-
-subtotal = cantidad_productos * precio_unitario
-
-if porcentaje_descuento > 0:
-    costo_total = subtotal * (1 - porcentaje_descuento/100)
-else: 
-    costo_total = subtotal
-
-print(f"Nombre del producto: {nombre_producto}")
-print(f"cantidad del producto: {cantidad_productos}")
-print(f"total a pagar: {costo_total: .2f}")
-
-
-
-    
-
-          
     
