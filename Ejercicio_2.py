a0f767c329b8b0c92eaf2e4fd53ec4c0df09a020
MigueLA207 <mariasmarin981@gmail.com>
2025-04-24 09:04:28 -0500

    Ejercicio_2.py

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -1,5 +1,4 @@
 #Verificar si un número está en una lista
-'''
 lista = [1,2,3,4,5]
 
 num= int(input("Escriba el valor de un numero: "))
@@ -8,20 +7,6 @@ if num in lista:
     print("El numero esta en la lista")
 else:
     print("El numero no se encuentra ne la lista")
-    '''
-productos = [["Arroz",2000,3,10],["huevo",700,5,5]]
-total = 0
-
-for valor in productos:
-    subtotal = 0
-    descuento = 0
-    print(valor[0],valor[1],valor[2])
     
-    subtotal = (subtotal + (valor[1]*valor[2]))
-    descuento = subtotal * (valor[3]/100)
-    total = subtotal-descuento
-    
-
-print(total)
 
 
